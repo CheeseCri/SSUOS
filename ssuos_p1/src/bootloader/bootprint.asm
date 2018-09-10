a1e273ad709309479c7b02c03885baae562f7471
CheeseCri <yunghn3@gmail.com>
2018-09-10 21:06:30 +0900

    PR1 PR2 주석 처리 완료

--- ssuos_p1/src/bootloader/bootprint.asm
+++ ssuos_p1/src/bootloader/bootprint.asm
@@ -1,22 +1,22 @@
 [BITS 16]
 
 START:   
-mov		ax, 0xb800
-mov		es, ax
-mov		ax, 0x00
-mov		bx, 0
-mov		cx, 80*25*2
+mov		ax, 0xb800	;비디오 메모리 지정.
+mov		es, ax		;es에 비디오 메모리 저장
+mov		ax, 0x00 	;ax 0으로 초기화
+mov		bx, 0 		;bx 0으로 초기화
+mov		cx, 80*25*2 ;화면 크기가 80*25이므로 80 * 25에 글자 용량이 2byte이기에 *2를 붙임.
 
 
 CLS:
-mov		[es:bx], ax
-add		bx, 1
-loop 	CLS
+mov		[es:bx], ax ;es:bx 위치에 ax의 값, 0을 넣음
+add		bx, 1		;그 이후 bx의 값을 1 증가 시킴
+loop 	CLS			;bx의 값이 80 * 25 * 2가 될때까지 루프
 
-mov		ah, 0x09
-mov 		al, 'H'
-mov		[es:0000], ax
-mov		al, 'e'
+mov		ah, 0x09	;ax의 왼쪽 8비트 레지스터에 문자 양식 저장.
+mov 	al, 'H'		;ax의 오른쪽 8비트 레지스터에 문자 아스키 코드 저장.
+mov		[es:0000], ax ;ax 레지스터를 비디오 메모리 주소에 넣음으로서 글자를 출력함.
+mov		al, 'e'		  ;이후 이 과정을 반복하여 "Hello, Keonteck's World"를 화면에 출력함.
 mov     [es:0002], ax
 mov		al, 'l'
 mov		[es:0004], ax
